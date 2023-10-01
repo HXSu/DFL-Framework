b1a62d4d7a65b16e065d0f3081f3db001cf96fdc
Hongxu <125354300+HXSu@users.noreply.github.com>
2023-10-01 13:47:57 +0800

    Update SMPC_scheduler.py

--- DFL_framework/SMPC_scheduler.py
+++ DFL_framework/SMPC_scheduler.py
@@ -3,65 +3,119 @@ This is a scheduler for SMPC process.
 SMPC could make the uploaded model more secure.
 """
 from transfer_manager import TransferManager
-# from transformers import AutoModelForSeq2SeqLM
-from transformers import GPT2LMHeadModel
-import torch
-import numpy as np
+from func_timeout import func_timeout, FunctionTimedOut
+from utils.helper_functions import *
 import os
+import numpy as np
+import torch
+
+from transformers import (
+    AutoConfig,
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+)
+from peft import *
 
 tm = TransferManager()
 stage = tm.stage
 
+BASE_MODEL_NAME = 'FieldSu/distil_student_24'
+MODE_CAHE = './base_model'
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
 def partial_and_upload(): # SMPC partial encryption and upload
     print("Start splitting and uploading partial model.")
-    partial_hash_code = []
-    approved_nodes_length = tm.approved_nodes_length
-    model_state_dict = torch.load(f"{tm.config['my_trained']}/my_trained_model_state.pt") # load my trained model state
-    print(model_state_dict["transformer.h.0.ln_1.weight"]) ###############
-    for i in range(approved_nodes_length-1): # randomly generate partial models
+    # approved_nodes_length = tm.approved_nodes_length
+    appr_nodes_arr = tm.approved_nodes_arr
+
+    model = AutoModelForCausalLM.from_pretrained(
+        BASE_MODEL_NAME,
+        # return_dict=True,
+        # quantization_config=bnb_config,
+        device_map="auto",
+        trust_remote_code=True,
+        cache_dir=MODE_CAHE
+    )
+
+    my_adp = tm.config["my_trained"] + '/my_finetuned_adp/'
+
+    model = PeftModel.from_pretrained(model, my_adp, cache_dir=MODE_CAHE)
+
+    model_state_dict = model.state_dict()
+
+
+    for i in appr_nodes_arr: # randomly generate partial models
+        if i.lower() == tm.config["my_address"].lower():
+            continue
+
+        key_fingerprint = tm.get_key_fingerprint(i) # get key fingerprint of the node
+
         model_state_dict_rand = {}
         for key in model_state_dict.keys():
-            model_state_dict_rand[key] = torch.from_numpy(np.random.rand(*model_state_dict[key].shape))
+            model_state_dict_rand[key] = torch.from_numpy(np.random.rand(*model_state_dict[key].shape)).to(device)
             model_state_dict[key] -= model_state_dict_rand[key]
-        print(model_state_dict_rand["transformer.h.0.ln_1.weight"]) ###############
-        partial_path = f"{tm.config['partial_storage']}/partial_model_{i}.pt"
-        torch.save(model_state_dict_rand, partial_path)
-        partial_hash_code.append(tm.upload_file_to_IPFS(partial_path))
 
-    tm.upload_partial_model(partial_hash_code) # upload partial models to blockchain
-    torch.save(model_state_dict, f"{tm.config['recombination_storage']}/partial_model_remain.pt") # keep the remained part
-    print(model_state_dict["transformer.h.0.ln_1.weight"]) ###################
+        partial_path = f"{tm.config['partial_storage']}/partial_model_{i}.pt"
+        torch.save(model_state_dict_rand, partial_path) # save partial models
+
+        part_hash = tm.upload_file_to_IPFS(partial_path) # upload partial models to IPFS
+        encrypted_part_hash = tm.encrypt_message(part_hash, key_fingerprint) # encrypt partial models' hash code
+
+        tm.upload_partial_model(i, str(encrypted_part_hash)) # upload partial models' hash code to blockchain
+
+    remain_path = f"{tm.config['recombination_storage']}/partial_model_remain.pt"
+    torch.save(model_state_dict, remain_path) # save my remained part
+
     print("Uploaded!!!")
 
 def download_and_recombine(): # SMPC download and recombine then upload
     print("Start downloading partial models")
     my_distributed_partial = tm.partial_model_rcd
     for i in range(len(my_distributed_partial)): # download all partial models belonging to me
-        hash = my_distributed_partial[i]
-        tm.download_file_from_IPFS(hash, f"{tm.config['recombination_storage']}/model_partial_R{i}.pt")
+        encrypted_part_hash = my_distributed_partial[i]
+        hash = tm.decrypt_message(encrypted_part_hash) # decrypt partial models' hash code
+
+        dl_partial_state = f"{tm.config['recombination_storage']}/partial_model_state_{i}.pt"
+        tm.download_file_from_IPFS(hash, dl_partial_state)
 
     print("Downloaded!!!")
     print("Start recombining partial models")
     recomb_list = os.listdir(tm.config["recombination_storage"])
 
-    model_state_dict = torch.load(f"{tm.config['recombination_storage']}/{recomb_list[0]}") # recombine partial models and my remained
-    # print(model_state_dict["transformer.h.0.ln_1.weight"]) ###################
-    for i in range(1, len(recomb_list)):
-        model_state_dict_i = torch.load(f"{tm.config['recombination_storage']}/{recomb_list[i]}")
-        # print(model_state_dict_i["transformer.h.0.ln_1.weight"]) ###################
-        for key in model_state_dict:
-            model_state_dict[key] += model_state_dict_i[key]
+    model = AutoModelForCausalLM.from_pretrained(
+        BASE_MODEL_NAME,
+        # return_dict=True,
+        # quantization_config=bnb_config,
+        device_map="auto",
+        trust_remote_code=True,
+        cache_dir=MODE_CAHE
+    )
 
-    recomb_state_path = f"{tm.config['recombination_storage']}/final_recomb_model_state.pt"
-    torch.save(model_state_dict, recomb_state_path) # save the recombination
+    glob_adp = tm.config["global_storage"]+'/glob_adp/'
+    model = PeftModel.from_pretrained(model, glob_adp, cache_dir=MODE_CAHE)
+
+    sum_state = {}
+    for i in range(len(recomb_list)): # load all partial models
+        model_state_dict_i = torch.load(f"{tm.config['recombination_storage']}/{recomb_list[i]}")
+        for key in model_state_dict_i:
+            if i == 0:
+                sum_state[key] = model_state_dict_i[key]
+            else:
+                sum_state[key] += model_state_dict_i[key]
+
+    final_recomb_path = f"{tm.config['recombination_storage']}/final_recomb_model.pt"
+    torch.save(sum_state, final_recomb_path) # recombine partial models and my remained
+
     print("Recombined!!!")
 
-    recomb_state_hash = tm.upload_file_to_IPFS(recomb_state_path) # upload the recombination to IPFS
-    tm.stack_ready_for_merge(recomb_state_hash)  # stack the recombination to the blockchain
+    recomb_hash = tm.upload_file_to_IPFS(final_recomb_path) # upload the recombination to IPFS
+    tm.stack_ready_for_merge(recomb_hash) # stack the recombination to the blockchain
     print("Uploaded to IPFS and stacked to blockchain!!!")
 
-    if os.path.exists(recomb_state_path): # remove the recombination
-        os.remove(recomb_state_path)
+    if os.path.exists(final_recomb_path): # remove the recombination
+        os.remove(final_recomb_path)
 
 if __name__ == '__main__':
     reg = tm.register_status
