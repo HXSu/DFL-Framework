064888b593cdc31bdce0533ac69d478517f0eeaa
Hongxu <125354300+HXSu@users.noreply.github.com>
2023-10-01 11:19:43 +0800

    Update deploy_nodes.py

--- deploy_nodes.py
+++ deploy_nodes.py
@@ -12,6 +12,8 @@ import shutil
 import json
 import argparse
 
+from datasets import load_dataset, DatasetDict
+
 def parse_args(): # parse arguments
     parser = argparse.ArgumentParser(description="Deploy nodes according to config file.")
     parser.add_argument( # specify the directory to deploy nodes
@@ -32,8 +34,8 @@ def deploy():
 
     if os.path.exists("./DFL_framework/model_storage"): # clear model_storage first
         shutil.rmtree("./DFL_framework/model_storage") 
-    if os.path.exists(f"./{args.deploy_dir}"): # clear deploy_dir first
-        shutil.rmtree(f"./{args.deploy_dir}")
+    if os.path.exists(f"{args.deploy_dir}"): # clear deploy_dir first
+        shutil.rmtree(f"{args.deploy_dir}")
 
     for i in range(deploy_config["nodes_num"]): # create nodes according to config and write config.json in each node
         shutil.copytree("./DFL_framework", f"{args.deploy_dir}/node{i}/DFL_framework") # distribute DFL_framework to each node
@@ -53,6 +55,30 @@ def deploy():
         with open(f"{args.deploy_dir}/node{i}/DFL_framework/config/config.json", "w") as f_i: # rewrite config.json
             json.dump(config_i, f_i, indent=4)
 
+
+
+    # ========================================= distribute sample data =========================================
+    distribute_num = deploy_config["nodes_num"]
+    node_folder_dirs = os.listdir(f"{args.deploy_dir}") # get all node folder dirs
+    # load all data first
+    all_data_path = "./sample_data/sample_data60000.parquet"
+    all_data = load_dataset("parquet", data_files=all_data_path, split="train")
+
+    # split all data into distribute_num parts and distribute to each node folder
+    split_length = len(all_data) // distribute_num
+
+    # train_test_ratio = 0.8
+    for i in range(distribute_num):
+        part_data = load_dataset("parquet", data_files=all_data_path, split=f"train[{i*split_length}:{(i+1)*split_length}]")
+        # train_test_dict = part_data.train_test_split(train_size=train_test_ratio, test_size=1-train_test_ratio)
+
+        data_storage = f"{args.deploy_dir}/{node_folder_dirs[i]}/DFL_framework/all_data/"
+        if not os.path.exists(data_storage):
+            os.makedirs(data_storage)
+
+        train_test_set_path = data_storage + f"train_test{split_length}.parquet"
+
+        part_data.to_parquet(train_test_set_path)
 if __name__ == "__main__":
     deploy()
     exit(0)
